6878f58d39c8cb8a0ee4652da0d3e80766ad0238
ML-projects <titustraining4@gmail.com>
2020-07-12 22:35:40 +0530

    Updated the paths for images and csv file

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     util.graphForMonths()
-    return render_template('index.html', linePlotPath="\static\images\graph.png")
+    return render_template('index.html', linePlotPath="\static\images\graph.png", sample="\static\images\sample.png")
     
 @app.route('/predict', methods=['POST'])
 def predict():
